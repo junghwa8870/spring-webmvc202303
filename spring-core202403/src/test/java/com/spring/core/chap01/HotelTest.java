b27fb399b4d6fa41ca8d3ed37c2323f89e8b9223
jeonghwa <suejunghwa@gmail.com>
2024-03-22 13:24:16 +0900

    #2. 스프링 IoC, DI

---
+++ spring-core202403/src/test/java/com/spring/core/chap01/HotelTest.java
@@ -0,0 +1,22 @@
+package com.spring.core.chap01;
+
+import com.spring.core.config.HotelManager;
+import org.junit.jupiter.api.Test;
+
+class HotelTest {
+
+    @Test
+    void hotelTest() {
+//        Chef chef = new JohnChef();
+//        Restaurant res = new WesternRestaurant(chef);
+//        Hotel hotel = new Hotel(res, chef);
+
+//        HotelManager manager = new HotelManager();
+//        manager.hotel().reserve();
+
+
+    }
+
+
+
+}
