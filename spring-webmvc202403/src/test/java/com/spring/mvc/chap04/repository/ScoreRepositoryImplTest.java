1d775c50b341cbd87159390dc73ea8e9b2c526e8
jeonghwa <suejunghwa@gmail.com>
2024-03-27 17:13:23 +0900

    scorerepository

--- spring-webmvc202403/src/test/java/com/spring/mvc/chap04/repository/ScoreRepositoryImplTest.java
+++ spring-webmvc202403/src/test/java/com/spring/mvc/chap04/repository/ScoreRepositoryImplTest.java
@@ -32,14 +32,13 @@ class ScoreRepositoryImplTest {
     @Test
     @DisplayName("tbl_score의 모든 학생 목록을 조회했을 때 학생의 수는 4명일 것이다.")
     void findAllTest() {
-        // given 테스트에 필요한 초기 설정이나 데이터를 설정합니다. 주로 조회할 데이터의 식별자를 설정합니다.
-        String sort = "name"; // 예시로 이름순으로 정렬하는 것을 가정한다.
-
-        // when 테스트할 메소드를 호출하고, 해당 메소드가 실행되는 경우의 동작을 정의합니다. 주로 데이터베이스에서 데이터를 조회하는 메소드를 호출합니다.
+        // given
+        String sort = "num";
+        // when
         List<Score> scoreList = repository.findAll(sort);
         System.out.println("scoreList = " + scoreList);
 
-        // then 메소드 호출 결과와 예상 결과를 비교하여 테스트를 수행합니다. 메소드가 정상적으로 동작하고 예상 결과와 일치하는지 확인합니다.
+        // then
         assertEquals(4, scoreList.size());
     }
 
